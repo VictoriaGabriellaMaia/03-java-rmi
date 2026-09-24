bc11c800e1fd555e43c1851ec5d81e6b94f9dfe2
Victoria Maia <victoria.gabms@gmail.com>
2026-09-24 15:20:28 -0300

    Atualiza Cliente para Utilizar Calculadora Remota

--- Cliente.java
+++ Cliente.java
@@ -1,22 +1,31 @@
-import java.rmi.*;
+import java.rmi.Naming;
 
 public class Cliente {
-  public static void main(String[] args) { 
-    try {
-       ServicoInterface servico = (ServicoInterface) Naming.lookup("rmi://localhost/ServiçoRemoto");
-       
-       System.out.println("Cliente operando!");
 
-       // Parâmetro que será passado para o método remoto:
-       Pi pi = new Pi();
+    public static void main(String[] args) {
 
-       // Chamada do método remoto:
-       Double dobroDePi = (Double) servico.executarTarefa(pi);
+        try {
+            // Aqui eu procuro o serviço da calculadora que foi registrado no RMI Registry.
+            ServicoInterface servico =
+                    (ServicoInterface) Naming.lookup("rmi://localhost/CalculadoraRemota");
 
-       System.out.println("Resposta do servidor - valor de 2 * PI: " + dobroDePi);
-    } catch (Exception e) {
-       System.err.println ("Erro no cliente: " + e.toString ()); 
-       e.printStackTrace ();
+            System.out.println("Cliente operando!");
+
+            // Aqui eu faço uma chamada remota para cada operação da calculadora.
+            double resultadoSoma = servico.somar(10, 5);
+            double resultadoSubtracao = servico.subtrair(10, 5);
+            double resultadoMultiplicacao = servico.multiplicar(10, 5);
+            double resultadoDivisao = servico.dividir(10, 5);
+
+            // Aqui eu mostro os resultados recebidos do servidor.
+            System.out.println("Resultado da soma: " + resultadoSoma);
+            System.out.println("Resultado da subtração: " + resultadoSubtracao);
+            System.out.println("Resultado da multiplicação: " + resultadoMultiplicacao);
+            System.out.println("Resultado da divisão: " + resultadoDivisao);
+
+        } catch (Exception e) {
+            System.err.println("Erro no cliente: " + e.toString());
+            e.printStackTrace();
+        }
     }
-  }
 }
