4561805930005e0d34cb51eaf77a56d57ecf902a
Victoria Maia <victoria.gabms@gmail.com>
2026-09-24 15:17:47 -0300

    Implementa Operações da Calculadora Remota
    Added basic arithmetic operations: somar, subtrair, multiplicar, and dividir.

--- Servico.java
+++ Servico.java
@@ -2,11 +2,24 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Servico extends UnicastRemoteObject implements ServicoInterface {
-  public Servico () throws RemoteException {
-    super ();
-  }
 
-  public Object executarTarefa (Parametro parametro) throws RemoteException {
-    return 2 * (Double) parametro.executar();
-  }
+    public Servico() throws RemoteException {
+        super();
+    }
+
+    public double somar(double numero1, double numero2) throws RemoteException {
+        return numero1 + numero2;
+    }
+
+    public double subtrair(double numero1, double numero2) throws RemoteException {
+        return numero1 - numero2;
+    }
+
+    public double multiplicar(double numero1, double numero2) throws RemoteException {
+        return numero1 * numero2;
+    }
+
+    public double dividir(double numero1, double numero2) throws RemoteException {
+        return numero1 / numero2;
+    }
 }
