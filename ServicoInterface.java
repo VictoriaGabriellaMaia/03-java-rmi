8051dc12f3a25eb603f887fef8ce13187d7b7c65
Victoria Maia <victoria.gabms@gmail.com>
2026-09-24 15:16:42 -0300

    Atualiza Interface para Calculadora Remota

--- ServicoInterface.java
+++ ServicoInterface.java
@@ -2,5 +2,12 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ServicoInterface extends Remote {
-  Object executarTarefa (Parametro parametro) throws RemoteException;
+
+    double somar(double numero1, double numero2) throws RemoteException;
+
+    double subtrair(double numero1, double numero2) throws RemoteException;
+
+    double multiplicar(double numero1, double numero2) throws RemoteException;
+
+    double dividir(double numero1, double numero2) throws RemoteException;
 }
